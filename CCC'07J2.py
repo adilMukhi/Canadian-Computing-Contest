5750db4ddb517e7a8664ecc6d82678661334a3d0
Adil Mukhi <mukhiadil2009@gmail.com>
2024-11-18 20:27:55 -0500

    2007 Junior CCC Questions

---
+++ CCC'07J2.py
@@ -0,0 +1,30 @@
+text = ""
+
+while text != "TTYL":
+    text = input()
+    if text == "CCC":
+        print("Canadian Computing Competition")
+    elif text == "CUZ":
+        print("because")
+    elif text == "TY":
+        print("Thank-you")
+    elif text == "YW":
+        print("You're welcome")
+    elif text == "CU":
+        print("see you")
+    elif text == "TA":
+        print("totally awesome")
+    elif text == ":-)":
+        print("I'm happy")
+    elif text == ":(":
+        print("I'm unhappy")
+    elif text == ";-)":
+        print("wink")
+    elif text == ":-P":
+        print("stick out my tongue")
+    elif text == "(~.~)":
+        print("sleepy")
+    else:
+        print(text)
+
+print("talk to you later")
